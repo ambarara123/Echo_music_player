263c1f9c1dc8f2f806d4ed4a7995169479be9b25
Ambar Kumar <ambar.ara123@gmail.com>
2018-06-15 08:20:00 +0530

    Navigation drawer with recyclerview ,now playing screen,Fragments added !

--- app/src/main/java/com/android/echo/activities/MainActivity.kt
+++ app/src/main/java/com/android/echo/activities/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.android.echo.activities
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
@@ -10,34 +9,53 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
 import com.android.echo.R
+import com.android.echo.adapters.RecyclerAdapter
 import com.android.echo.fragments.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
-    var drawerLayout :DrawerLayout?=null
+    var navDrawerIconList :ArrayList<String> = arrayListOf()
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    object Statified{
+        var drawerLayout :DrawerLayout?=null
+    }
+
+    var imageIcons = intArrayOf(R.drawable.navigation_allsongs,R.drawable.navigation_favorites
+    ,R.drawable.navigation_settings,R.drawable.navigation_aboutus)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        drawerLayout = findViewById(R.id.drawer_layout)
-        val toggle = ActionBarDrawerToggle(this@MainActivity,drawerLayout,toolbar,
-                R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        drawerLayout?.addDrawerListener(toggle)
-        toggle.syncState()
 
+        navDrawerIconList.add("All Songs")
+        navDrawerIconList.add("Favourites")
+        navDrawerIconList.add("Settings")
+        navDrawerIconList.add("About Us")
+
+        MainActivity.Statified.drawerLayout = findViewById(R.id.drawer_layout)
+        val toggle = ActionBarDrawerToggle(this@MainActivity,MainActivity.Statified.drawerLayout,toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        MainActivity.Statified.drawerLayout?.addDrawerListener(toggle)
+        toggle.syncState()
 
         val mainScreenFragment = MainFragment()
         this.supportFragmentManager.beginTransaction()
                 .add(R.id.detail_fragment, mainScreenFragment,"MainScreenFragment")
                 .commit()
 
-        var navigation_recyclerView = findViewById<RecyclerView>(R.id.nav_recyclerView)
+        var adapter = RecyclerAdapter(navDrawerIconList,imageIcons,this)
+        adapter.notifyDataSetChanged()
+
+        var navigation_recyclerView = findViewById<RecyclerView>(R.id.nav_recycler_view)
         navigation_recyclerView.layoutManager = LinearLayoutManager(this)
         navigation_recyclerView.itemAnimator = DefaultItemAnimator()
 
+
+        navigation_recyclerView.adapter = adapter
+        navigation_recyclerView.setHasFixedSize(true)
 
     }
 
---
+++ app/src/main/java/com/android/echo/adapters/RecyclerAdapter.kt
@@ -0,0 +1,97 @@
+package com.android.echo.adapters
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import android.widget.TextView
+import com.android.echo.R
+import com.android.echo.activities.MainActivity
+import com.android.echo.fragments.AboutUsFragment
+import com.android.echo.fragments.FavourateFragment
+import com.android.echo.fragments.MainFragment
+import com.android.echo.fragments.SettingFragment
+
+class RecyclerAdapter(contentList :ArrayList<String>,getImage :IntArray, context :Context) :RecyclerView.Adapter<RecyclerAdapter.NavViewHolder>(){
+
+    var contentList:ArrayList<String>?=null
+    var getImage :IntArray?=null
+    var context : Context?=null
+
+
+
+    init {
+        this.contentList = contentList
+        this.getImage = getImage
+        this.context = context
+    }
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NavViewHolder {
+
+        var itemView = LayoutInflater.from(parent?.context)
+                .inflate(R.layout.row_nav_list,parent,false)
+
+        var viewHolder = NavViewHolder(itemView)
+        return viewHolder
+
+    }
+
+    override fun getItemCount(): Int {
+
+        return (contentList as ArrayList).size
+    }
+
+    override fun onBindViewHolder(holder: NavViewHolder, position: Int) {
+
+        holder.navIcon?.setBackgroundResource(getImage?.get(position) as Int )
+        holder.navText?.setText(contentList?.get(position))
+
+        holder.navContainer?.setOnClickListener({
+            if (position== 0){
+
+                var mainScreenFragment = MainFragment()
+                ( context as MainActivity).supportFragmentManager.beginTransaction()
+                        .replace(R.id.detail_fragment,mainScreenFragment)
+                        .commit()
+
+            }else if (position == 1){
+                var favourateFragment = FavourateFragment()
+                ( context as MainActivity).supportFragmentManager.beginTransaction()
+                        .replace(R.id.detail_fragment,favourateFragment)
+                        .commit()
+
+            }else if (position == 2){
+                var settingFragment = SettingFragment()
+                ( context as MainActivity).supportFragmentManager.beginTransaction()
+                        .replace(R.id.detail_fragment,settingFragment)
+                        .commit()
+
+            }else{
+                var aboutUsFragment = AboutUsFragment()
+                ( context as MainActivity).supportFragmentManager.beginTransaction()
+                        .replace(R.id.detail_fragment,aboutUsFragment)
+                        .commit()
+            }
+            MainActivity.Statified.drawerLayout?.closeDrawers()
+        })
+    }
+
+    class NavViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+        var navIcon :ImageView?=null
+        var navText :TextView?=null
+        var navContainer :RelativeLayout?=null
+
+        init {
+            navIcon = itemView?.findViewById(R.id.icon_navDrawer)
+            navText = itemView?.findViewById(R.id.text_navDrawer)
+            navContainer = itemView?.findViewById(R.id.navigation_header_container)
+
+        }
+    }
+}
